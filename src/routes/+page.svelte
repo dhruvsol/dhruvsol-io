bf1e74376d29965513bab5470c596aadfd8af8a5
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2025-07-10 15:53:57 +0800

    feat: added base setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="flex items-top flex-col items-center justify-center pt-20">
+	<article class="max-w-3xl w-full text-white flex flex-col gap-10">
+		<header class="text-left">
+			<h1 class="text-3xl font-bold">Dhruvraj Solanki</h1>
+			<p class="text-lg text-gray-400">
+				<a href="http"> @dhruvsol </a>
+			</p>
+		</header>
+		<section>
+			<h2 class="text-2xl font-semibold mb-4">About Me</h2>
+			<p class="mb-4 leading-relaxed text-gray-400">
+				I’m a software engineer exploring blockchain infrastructure and DeFi.<br /> As the founder
+				of
+				<a class="font-bold underline" href="https://github.com/brewlabshq" target="_blank">
+					Brew Labs HQ
+				</a>, where we build open source projects around solana validator client and tools.
+			</p>
+			<p class="mb-4 leading-relaxed text-gray-400">
+				I operate <a class="font-bold underline" href="https://www.icestaking.com/" target="_blank"
+					>IceStaking</a
+				>, a high-performance Solana validator, and manage the
+				<a class="font-bold underline" href="https://www.dynosol.io" target="_blank">dynoSOL</a> stake
+				pool, to support ecosystem contributors.
+			</p>
+			<p class="leading-relaxed text-gray-400 italic">
+				Note: The “sol” in my handle stands for Solanki, not Solana—and sadly I can’t airdrop free
+				SOL. Also, I do not own dhruv.sol, but if you find him, send him my regards!
+			</p>
+		</section>
+		<section class="">
+			<header class="text-left">
+				<h2 class="text-2xl font-semibold mb-4">Builder's Log</h2>
+			</header>
+		</section>
+		<section class="">
+			<header class="text-left">
+				<h2 class="text-2xl font-semibold mb-4">Thinking Aloud</h2>
+			</header>
+		</section>
+	</article>
+</section>
