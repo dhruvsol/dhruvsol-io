c163c313277f50388a182a3563f60e93e3ed1d06
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2025-07-11 12:53:27 +0800

    feat: added builder & think out loud section

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,66 @@
-// place files you want to import through the `$lib` alias in this folder.
+interface ThinkingAloud {
+	type: string;
+	link: string;
+	header: string;
+	content: string;
+	date: Date;
+}
+
+export const THINKING_ALOUD: ThinkingAloud[] = [
+	{
+		type: 'github',
+		content: 'A guide for setting up validators, blazingly fast.',
+		date: new Date('2024-11-03'),
+		header: 'Validator Jumpstart',
+		link: 'https://github.com/brewlabshq/validator-jumpstart'
+	},
+	{
+		type: 'blog',
+		content: 'Voting Modifications and Validator Voting Behavior Study V 1.0',
+		date: new Date('2025-04-12'),
+		header: 'Voting Modifications & Voting',
+		link: 'https://medium.com/@hrknsinst/voting-modifications-and-validator-voting-behavior-study-v-1-0-14098cd01d0c'
+	}
+].sort((a, b) => b.date.getTime() - a.date.getTime());
+
+interface BuilderLog {
+	title: string;
+	content: string;
+	date: Date;
+	link: string;
+}
+
+export const BUILDER_LOG: BuilderLog[] = [
+	{
+		title: 'brewlabshq - Founder',
+		content: 'Espresso for Infra - home for open-source projects.',
+		date: new Date('2025-01-08'),
+		link: 'https://github.com/brewlabshq/'
+	},
+	{
+		title: 'dynoSOL - Core Team',
+		content:
+			'Stake for numbers and narratives - Stake your SOL with validators vetted by experts and on-chain performance',
+		date: new Date('2025-05-08'),
+		link: 'https://www.dynosol.io/'
+	},
+	{
+		title: 'Ice Staking',
+		content: 'High performance solana validator - Freeze your SOL for ice cold yields',
+		date: new Date('2024-04-12'),
+		link: 'https://www.icestaking.com/'
+	},
+	{
+		title: 'Cubik - Co-Founder/CTO',
+		content: 'Funding the future of @Solana, one builder at a time.',
+		date: new Date('2023-02-01'),
+		link: 'https://github.com/cubik-so/'
+	},
+	{
+		title: 'Superteam Earn - Software Engineer',
+		content:
+			'An open source platform connecting crypto founders with elite talent to create bounties, and accelerate project completion',
+		date: new Date('2023-01-01'),
+		link: 'https://github.com/SuperteamDAO/earn'
+	}
+].sort((a, b) => b.date.getTime() - a.date.getTime());
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,22 @@
+<script lang="ts">
+	import { BUILDER_LOG, THINKING_ALOUD } from '$lib';
+
+	const posts = [...THINKING_ALOUD];
+	const logs = [...BUILDER_LOG];
+	function formatDate(date: Date) {
+		return date.toLocaleDateString(undefined, {
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric'
+		});
+	}
+</script>
+
 <section class="flex items-top flex-col items-center justify-center pt-20">
 	<article class="max-w-3xl w-full text-white flex flex-col gap-10">
 		<header class="text-left">
 			<h1 class="text-3xl font-bold">Dhruvraj Solanki</h1>
-			<p class="text-lg text-gray-400">
+			<p class="text-md text-gray-400">
 				<a href="http"> @dhruvsol </a>
 			</p>
 		</header>
@@ -27,15 +41,91 @@
 				SOL. Also, I do not own dhruv.sol, but if you find him, send him my regards!
 			</p>
 		</section>
+
+		<div class="relative my-8">
+			<hr class="border-t border-gray-300" />
+
+			<div class="absolute inset-x-0 top-1/2 transform -translate-y-1/2 flex justify-center">
+				<div class="bg-primary px-2 text-gray-500">✕</div>
+			</div>
+		</div>
+
+		<section class="">
+			<header class="text-left">
+				<h2 class="text-2xl font-semibold mb-4">Thinking Aloud</h2>
+				<p class="text-md text-gray-400">
+					A digital junk drawer of my brain — tweets, blogs, code rants, and mildly chaotic
+					opinions. Mostly tech, occasionally coherent, always unfiltered. Proceed with curiosity
+					(and low expectations).
+				</p>
+			</header>
+			<ul class="mt-6 space-y-6">
+				{#each posts as post (post.link)}
+					<li class="rounded-lg list-disc">
+						<div class="flex justify-start gap-2">
+							<a
+								href={post.link}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="text-xl font-semibold text-white hover:underline"
+							>
+								{post.header}
+							</a>
+							<p class="text-xs text-gray-500 mt-2">{formatDate(post.date)}</p>
+						</div>
+						<p class="text-sm text-gray-400 mt-1">{post.content}</p>
+					</li>
+				{/each}
+			</ul>
+		</section>
+		<div class="relative my-8">
+			<hr class="border-t border-gray-300" />
+
+			<div class="absolute inset-x-0 top-1/2 transform -translate-y-1/2 flex justify-center">
+				<div class="bg-primary px-2 text-gray-500">✕</div>
+			</div>
+		</div>
 		<section class="">
 			<header class="text-left">
 				<h2 class="text-2xl font-semibold mb-4">Builder's Log</h2>
 			</header>
+			<ul class="mt-6 space-y-6">
+				{#each logs as log (log.link)}
+					<li class="rounded-lg list-disc">
+						<div class="flex justify-start gap-2">
+							<a
+								href={log.link}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="text-xl font-semibold text-white hover:underline"
+							>
+								{log.title}
+							</a>
+							<p class="text-xs text-gray-500 mt-2">{formatDate(log.date)}</p>
+						</div>
+						<p class="text-sm text-gray-400 mt-1">{log.content}</p>
+					</li>
+				{/each}
+			</ul>
 		</section>
-		<section class="">
+		<div class="relative my-8">
+			<hr class="border-t border-gray-300" />
+
+			<div class="absolute inset-x-0 top-1/2 transform -translate-y-1/2 flex justify-center">
+				<div class="bg-primary px-2 text-gray-500">✕</div>
+			</div>
+		</div>
+		<footer class="text-white">
 			<header class="text-left">
-				<h2 class="text-2xl font-semibold mb-4">Thinking Aloud</h2>
+				<h2 class="text-2xl font-semibold mb-4">Contract me</h2>
 			</header>
-		</section>
+
+			<div class="flex justify-start underline mb-10 gap-3">
+				<a target="_blank" href="https://x.com/dhruvsol_">X</a>
+				<a target="_blank" href="mailto:me@dhruvsol.io">Email</a>
+				<a target="_blank" href="https://substack.com/@dhruvsol">Substack</a>
+				<a target="_blank" href="https://github.com/dhruvsol">Github</a>
+			</div>
+		</footer>
 	</article>
 </section>
